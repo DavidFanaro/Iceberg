4bbeb0bacab20a2aa7da2766f099e92da81765d0
David Fanaro <fanarodavid@gmail.com>
2022-06-02 14:15:41 -0400

    added form with formik and worked on the service page.

---
+++ pages/services.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import AddMenu from '../components/ServicesMenus/AddMenu';
+import EditMenu from '../components/ServicesMenus/EditMenu';
+
+
+enum Page {
+  ADD,
+  EDIT,
+}
+
+const services = () => {
+
+  const [currentPage, setcurrentPage] = useState<Page>(Page.ADD);
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='w-5/6 h-5/6 bg-slate-100 rounded-3xl p-3'>
+        <div className='flex justify-around mt-4'>
+          <button type='button' onClick={() => setcurrentPage(Page.ADD)}>
+            ADD
+          </button>
+          <button type='button' onClick={() => setcurrentPage(Page.EDIT)}>
+            edit
+          </button>
+        </div>
+        {currentPage === Page.ADD ? <AddMenu /> : <EditMenu />}
+      </div>
+    </div>
+  )
+}
+
+export default services
