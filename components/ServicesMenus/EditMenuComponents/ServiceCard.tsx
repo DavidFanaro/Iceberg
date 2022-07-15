45623ea5edd75d5425a09e10ac42ad591869f01c
David Fanaro <fanarodavid@gmail.com>
2022-07-15 01:55:28 -0400

    Cleaned up services page

--- components/ServicesMenus/EditMenuComponents/ServiceCard.tsx
+++ components/ServicesMenus/EditMenuComponents/ServiceCard.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import React from 'react'
 import { FiEdit } from 'react-icons/fi'
 
@@ -12,16 +11,18 @@ type Props = {
 
 const ServiceCard = ({ id, name, image }: Props) => (
   <div className='w-full bg-slate-300 rounded-md p-3 flex justify-between px-3'>
-    <div className='flex flex-row gap-3'>
+    <div className='flex flex-row gap-3 items-center'>
       <img src={image} alt={name} className=' h-16' />
       <div>
         {name}
       </div>
     </div>
-    <a type='button' className='btn btn-sm gap-2' href={`/editservice/${id}`}>
-      <FiEdit />
-      Edit
-    </a>
+    <div className='flex items-center'>
+      <a type='button' className='btn btn-lg gap-2' href={`/editservice/${id}`}>
+        <FiEdit />
+        Edit
+      </a>
+    </div>
   </div>
 )
 
